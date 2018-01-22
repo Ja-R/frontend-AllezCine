d0f014db7ef3e0b6b21c2f1a56d75b9ad96b8bdc
nicolashacala <nicolas.hacala@gmail.com>
2018-01-22 12:35:50 +0100

    lunch break

---
+++ owlcarousel.js
@@ -0,0 +1,18 @@
+//Carousel
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    slideBy: 'page'
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
