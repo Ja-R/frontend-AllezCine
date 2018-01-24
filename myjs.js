fcac5b71ba4a23e39d015906ecbe45bcb59c57c7
nicolashacala <nicolas.hacala@gmail.com>
2018-01-24 16:52:34 +0100

    eod wednesday 24/01

--- myjs.js
+++ myjs.js
@@ -1,4 +1,10 @@
-let featuredMovies = [
+let featuredMoviesContainer = document.getElementById('featuredMoviesContainer');
+let featuredSeriesContainer = document.getElementById('featuredSeriesContainer');
+var moviesContainerChildren = featuredMoviesContainer.childNodes;
+let show = document.getElementById('showMoreOrLessMovies');
+let allMoviesDisplayed = false;
+
+const featuredMovies = [
     {title: 'Winchester', genre: 'Horror', year: 2018},
     {title: 'A Fantastic Woman', genre: 'Drama', year: 2018},
     {title: 'The Cage Fighter', genre: 'Action', year: 2018},
@@ -18,3 +24,86 @@ let featuredMovies = [
     {title: 'Wild Wild West', genre: 'Fantasy', year: 1999},
     {title: 'Ghostbusters II', genre: 'Fantasy', year: 1989}
 ];
+
+let displayFeatured = function (container, data, bool, button){
+    bool = false;
+    button.style.display = 'inline'
+    button.innerHTML = 'Show More';
+    container.innerHTML = "";
+    for(let i=0; i<data.length; i++){
+        let description = document.createTextNode(data[i].title + ' ' + data[i].genre + ' ' + data[i].year);
+        
+        let wrap = document.createElement('DIV');
+        wrap.className = 'col-xl-2';
+        wrap.appendChild(description);
+        
+        if(i>=12){
+            wrap.style.display = 'none';
+        }
+
+        container.appendChild(wrap);
+    }
+}
+let showAllMovies = document.getElementById('showAllMovies');
+showAllMovies.addEventListener('click', function(){
+    displayFeatured(featuredMoviesContainer, featuredMovies, allMoviesDisplayed, show);
+});
+
+let showMoreOrLessMovies = function(){
+    if(allMoviesDisplayed == false){
+        for(let i=1; i<moviesContainerChildren.length; i++){
+            moviesContainerChildren[i].style.display = 'inline';
+        }
+        allMoviesDisplayed = true;
+        showMovies.innerHTML = 'Show Less';
+    }
+    else if(allMoviesDisplayed == true){
+        for(let i=12; i<moviesContainerChildren.length; i++){
+            moviesContainerChildren[i].style.display = 'none';
+        }
+        allMoviesDisplayed = false;
+        showMovies.innerHTML = 'Show More'
+    }
+}
+
+showMovies.addEventListener('click', showMoreOrLessMovies);
+
+let genreFilter = function(filter, container, data){
+    container.innerHTML = "";
+    showMovies.style.display = 'none';
+    for(let i=0; i<data.length; i++){
+        if(data[i].genre == filter){
+            let description = document.createTextNode(data[i].title + ' ' + data[i].genre + ' ' + data[i].year);
+            
+            let wrap = document.createElement('DIV');
+            wrap.className = 'col-xl-2';
+            wrap.appendChild(description);
+
+        container.appendChild(wrap);
+        }
+    }
+
+}
+let showAllActionMovies = document.getElementById('showAllActionMovies');
+showAllActionMovies.addEventListener('click', function(){
+    genreFilter('Action', featuredMoviesContainer, featuredMovies);
+});
+let showAllAdventureMovies = document.getElementById('showAllAdventureMovies');
+showAllAdventureMovies.addEventListener('click', function(){
+    genreFilter('Adventure', featuredMoviesContainer, featuredMovies);
+});
+let showAllComedyMovies = document.getElementById('showAllComedyMovies');
+showAllComedyMovies.addEventListener('click', function(){
+    genreFilter('Comedy', featuredMoviesContainer, featuredMovies);
+});
+let showAllFantasyMovies = document.getElementById('showAllFantasyMovies');
+showAllFantasyMovies.addEventListener('click', function(){
+    genreFilter('Fantasy', featuredMoviesContainer, featuredMovies);
+});
+let showAllHorrorMovies = document.getElementById('showAllHorrorMovies');
+showAllHorrorMovies.addEventListener('click', function(){
+    genreFilter('Horror', featuredMoviesContainer, featuredMovies);
+});
+
+displayFeatured(featuredMoviesContainer, featuredMovies, allMoviesDisplayed, showMovies);
+displayFeatured(featuredSeriesContainer, featuredMovies, allMoviesDisplayed, showSeries);
