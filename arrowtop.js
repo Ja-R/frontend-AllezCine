df7135dcc62b889a0050490e9a581342bb4a85dd
Ja-R <jacqueline_rwa@msn.com>
2018-01-25 20:39:48 +0100

    c+

--- arrowtop.js
+++ arrowtop.js
@@ -28,19 +28,6 @@ $(document).ready(function(){
   });
 
 
-//     // video dans modal
-//     $('.modalFilms').click(function () {
-//     var src = 'https://www.youtube.com/embed/whFAsePy-w8';
-//     $('#ModalFilm1').modal('show');
-//     $(' iframe').attr('src', src);
-//     });
-//
-//     $('#ModalFilm1 .btn').click(function () {
-//         $('#myModal iframe').removeAttr('src');
-//     });
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/whFAsePy-w8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-
-// });
 
 
 
