4f5687812bd12229dfc89e6ad1ce15891772f9ad
nicolashacala <nicolas.hacala@gmail.com>
2018-01-29 19:14:12 +0100

    goodies js done

--- jcvd.js
+++ jcvd.js
@@ -1,15 +1,22 @@
-
-$("input[type=number]").on('keyup input' || 'change' || 'mousewheel' || 'load', function(){
+let subTotalField = document.getElementById('subTotalField');
+let deliveryFeesField = document.getElementById('deliveryFeesField');
+$("input[type=number]").on('keyup input' || 'change' || 'mousewheel', function(){
     let subTotal = parseInt($('input[type=number]').val())*10;
     if(subTotal <= 15){
         let total = subTotal + 10;
-        $('#total').html('Total: ' + total + ' euros (10 euros de frais de livraisons inclus).');
+        subTotalField.innerHTML = 'Sous-Total: ' + subTotal + ' euros';
+        deliveryFeesField.innerHTML = 'Frais de Ports: 10 euros';
+        $('#total').html('Total: ' + total + ' euros');
     }
     else if(subTotal >= 16 && subTotal <= 30){
         let total = subTotal + 7;
-        $('#total').html('Total: ' + total + ' euros (7 euros de frais de livraisons inclus).');
+        subTotalField.innerHTML = 'Sous-Total: ' + subTotal + ' euros';
+        deliveryFeesField.innerHTML = 'Frais de Ports: 7 euros';
+        $('#total').html('Total: ' + total + ' euros');
     }
     else if(subTotal >= 31){
-        $('#total').html('Total: ' + subTotal + ' euros (Les frais de ports sont offerts!).');
+        subTotalField.innerHTML = 'Sous-Total: ' + subTotal + ' euros';
+        deliveryFeesField.innerHTML = 'Frais de Ports: Offerts!';
+        $('#total').html('Total: ' + subTotal + ' euros');
     }
 });
