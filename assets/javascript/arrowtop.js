e32fd85ba75d418b80b0dd12af9ef6d93bd9cc30
Ja-R <jacqueline_rwa@msn.com>
2018-02-02 12:32:46 +0100

    popup entree

--- assets/javascript/arrowtop.js
+++ assets/javascript/arrowtop.js
@@ -1,33 +1,40 @@
-// $(document).ready(function(){
-
-  // fleche vers le haut
-  var banniere = document.getElementById('banniere');
-  var arrow = document.getElementById('arrow');
-  $(window).scroll(function(){
-    // console.log('page', window.pageYOffset);
-    // console.log('banniere', banniere.offsetHeight);
-    if(window.pageYOffset > (banniere.offsetHeight / 2 )){
-      $(arrow).show();
-      // console.log('visible');
-    }
-    else{
-      $(arrow).hide();
-      // console.log('cache');
-    }
-  })
-
-
-  // shadow
-  function luminosite(){
-    $(".btn-trailer").mouseenter(function(){
-      $(".carousel-item img").removeClass("shadow").addClass("lightness");
-    });
-
-    $(".btn-trailer").mouseleave(function(){
-      $("#carouselHeader img").removeClass("lightness").addClass("shadow");
-    });
+// fleche vers le haut
+var banniere = document.getElementById('banniere');
+var arrow = document.getElementById('arrow');
+$(window).scroll(function(){
+  // console.log('page', window.pageYOffset);
+  // console.log('banniere', banniere.offsetHeight);
+  if(window.pageYOffset > (banniere.offsetHeight / 2 )){
+    $(arrow).show();
+    // console.log('visible');
   }
-  luminosite();
+  else{
+    $(arrow).hide();
+    // console.log('cache');
+  }
+})
 
 
-// });
+// shadow
+function luminosite(){
+  $(".btn-trailer").mouseenter(function(){
+    $(".carousel-item img").removeClass("shadow").addClass("lightness");
+  });
+
+  $(".btn-trailer").mouseleave(function(){
+    $("#carouselHeader img").removeClass("lightness").addClass("shadow");
+  });
+}
+luminosite();
+
+
+//modal d'autorisation
+
+$(document).ready(function() {
+    $("#themodal").modal("show");
+    console.log("clicko");
+});
+
+$(".close").click(function(){
+    $("#themodal").modal("hide");
+});
